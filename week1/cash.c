ba9110ce3d8f410ba6d4091e8519ac959157c68f
Lionel Ngoubou <38511971+ngoubou@users.noreply.github.com>
2020-11-16 11:12:54 +0100

    Update cash.c

--- week1/cash.c
+++ week1/cash.c
@@ -14,15 +14,16 @@ int main(void)
     
     // prompt the user to enter a positive float; 
     // if he fails to, re-prompt him for a valid input
-
     do
     {
         
         dollars = get_float("How much: ");
     }
     while (dollars < 0);
+ 
     // to handle float imprecision, we round the value to 100
     int change = round(dollars * 100);
+ 
     // as long as the change is greater or equals to the largest coin, use it
     // and count the number of times you do an operation, ie number of coins used
     int coins = 0;
@@ -31,16 +32,19 @@ int main(void)
         change = change - 25;
         coins++;
     }
+ 
     while (change >= 10)
     {
         change = change - 10;
         coins++;
     }  
+ 
     while (change >= 5)
     {
         change = change - 5;
         coins++;
     }
+ 
     while (change >= 1)
     {
         change = change - 1;
