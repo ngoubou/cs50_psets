099f73fe5712db13ac5230d85695a6f071253cf2
Lionel Ngoubou <38511971+ngoubou@users.noreply.github.com>
2020-11-29 22:09:01 +0100

    Create readability.c

---
+++ week2/readability.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+/*
+ * Author: Lionel Ngoubou
+ * Purpose: Coleman-Liau index implementation :
+  Compute the approximate grade level needed to comprehend some text
+ */
+
+ int main(void)
+ {
+  
+ }
