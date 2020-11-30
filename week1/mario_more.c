6c94a3e18e99d407f5b5b89f3b0d7a07c3bb6cd2
Lionel Ngoubou <38511971+ngoubou@users.noreply.github.com>
2020-11-30 17:05:23 +0100

    Update mario_more.c
    way easier than I initially thought. took me 20 minutes

--- week1/mario_more.c
+++ week1/mario_more.c
@@ -16,4 +16,38 @@ int main(void)
         height = get_int("Height: ");
     }
     while (height < 1 || height > 8);
+
+// at each line (iteration) i want n objects
+// these n objects are shared as follow
+// n - i - 1 spaces and the rest are hashes
+// where i is the level we're at (starting at 0)
+
+    for (int i = 0; i < height; i++)
+    {
+        // print the space characters
+        for (int space = height - 1 - i; space > 0 ; space--)
+        {
+            printf(" ");
+        }
+        // print the hashes
+        for (int hash = i + 1; hash > 0; hash--)
+        {
+            printf("#");
+
+        }
+        // print the in-between space
+        for (int j = 0; j < 2; j++)
+        {
+            printf(" ");
+        }
+        // print the 2nd pyramid
+        for (int pyr = i + 1; pyr > 0; pyr--)
+        {
+            printf("#");
+        }
+        
+        printf("\n");
+
+    }
+
 }
