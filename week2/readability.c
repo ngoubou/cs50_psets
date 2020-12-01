a5e3efc53cfeb77aff07f20f82514564ff08c0b6
Lionel Ngoubou <38511971+ngoubou@users.noreply.github.com>
2020-12-01 16:27:03 +0100

    use of functions
    I wrote and used functions to keep my code organized.

--- week2/readability.c
+++ week2/readability.c
@@ -9,50 +9,20 @@
  * Purpose: Coleman-Liau index implementation :
   Compute the approximate grade level needed to comprehend some text
  */
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
     // prompt user to enter a string
     string text = get_string("Text: ");
 
-    // number of letters
-    int len = strlen(text);
-    int letters = 0;
-    for (int i = 0; i != len; i++)
-    {
-        if (!isspace(text[i]) && isalnum(text[i]))
-        {
-            letters++;
-        }
-    }
-
-    // number of words
-    int words = 1;
-    for (int i = 0; i != len; i++)
-    {
-        if (isspace(text[i]) && !isalnum(text[i]))
-        {
-            words++;
-        }
-    }
-
-    // number of sentences
-    int sentences = 0;
-    for (int i = 0; i != len; i++)
-    {
-        // number of sentences
-        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
-        {
-            sentences++;
-        }
-    }
-
     //letters and sentences per 100 words
-    float l = (100 * (float) letters) / (float) words;
-    float s = (100 * (float) sentences) / (float) words;
+    float l = (100 * (float) count_letters(text)) / (float) count_words(text);
+    float s = (100 * (float) count_sentences(text)) / (float) count_words(text);
 
     int index = round(0.0588 * l - 0.296 * s - 15.8);
-
 
     if (index >= 16)
     {
@@ -66,7 +36,64 @@ int main(void)
     {
         printf("Grade %i\n", index);
     }
-
 }
 
+int count_letters(string text)
+{
+    int len = strlen(text);
+    int nchar = 0;
 
+    for (int i = 0; i != len; i++)
+    {
+        if (!isspace(text[i]) && isalnum(text[i]))
+        {
+            nchar++;
+        }
+    }
+    return nchar;
+}
+
+int count_words(string text)
+{
+    int len = strlen(text);
+    int nchar = 0;
+    int count = 0;
+
+    for (int i = 0; i != len; i++)
+    {
+        // number of letters
+        if (!isspace(text[i]) && isalnum(text[i]))
+        {
+            nchar++;
+        }
+        // number of words
+        if (isspace(text[i]) && !isalnum(text[i]))
+        {
+            count++;
+        }
+
+    }
+    return count + 1;
+}
+
+int count_sentences(string text)
+{
+    int len = strlen(text);
+    int nchar = 0;
+    int count = 0;
+
+    for (int i = 0; i != len; i++)
+    {
+        // number of letters
+        if (!isspace(text[i]) && isalnum(text[i]))
+        {
+            nchar++;
+        }
+        // number of sentences
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
+            count++;
+        }
+    }
+    return count;
+}
