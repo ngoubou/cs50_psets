1f811d35ef380c12d8f50100cebdbca5667bc72c
Lionel Ngoubou <38511971+ngoubou@users.noreply.github.com>
2020-12-03 21:20:57 +0100

    almost complete
    handles everything except non-numeric key

--- week2/caesar.c
+++ week2/caesar.c
@@ -15,37 +15,76 @@
 int main(int argc, string argv[])
 {
     string alph = {"abcdefghijklmnopqrstuvwxyz"};
+    
 
-    if (argv[1] == 0 || argc > 2) // check if arg is null
+    // check if arg is null
+    if (argv[1] == 0 || argc > 2) 
     {
-        printf("Usage: ./caesar key");
+        printf("Usage: ./caesar key\n");
+        return 1;
     }
+    // handles non numeric key
+   
 
     else
     {
+        // if it's not a number
+        // return 1 and exit
         int key =  atoi(argv[1]);
 
         if (key == 0)
         {
-            printf("Usage: ./caesar key");
+            printf("Usage: ./caesar key\n");
+            return 1;
         }
 
         else
         {
             string text = get_string("Gimme dat sauce: ");
 
-            //convert the input to an array so i can loop through
+            // i convert the input to an array so i can loop through
             string arr[] = {text};
 
             printf("ciphertext: ");
             for (int i = 0; i < strlen(arr[0]); i++)
             {
+                // check if it's space or nonalphanum and print the same thing
+                if (isspace(arr[0][i]) || !isalnum(arr[0][i]))
+                {
+                    printf("%c", arr[0][i]);
+                }
+
+
                 for (int j = 0; j < strlen(alph); j++)
                 {
+
+                    // if the characters are equals print the next one
                     if (arr[0][i] == alph[j])
                     {
-                        printf("%c", alph[j + key]);
+                        // if j + key is between 26 & 50, iterate at the start
+                        if (j + key > 25 && j + key < 51)
+                        {
+                            printf("%c", alph[j + key - 26]);
+                        }
+                        else if (j + key > 50)
+                        {
+                            printf("%c", alph[(j + key - 26) % 26]);
+                        }
+                        else
+                        {
+                            printf("%c", alph[j + key]);
+                        }
+
                     }
+                    // if it's a maj put the next one to maj
+                    else if (isupper(arr[0][i]))
+                    {
+                        if (toupper(arr[0][i]) == toupper(alph[j]))
+                        {
+                            printf("%c", toupper(alph[j + key]));
+                        }
+                    }
+
                 }
             }
 
