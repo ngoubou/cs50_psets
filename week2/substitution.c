b03310085280b0797c36b87e3ea9896dfcde7bcf
Lionel Ngoubou <38511971+ngoubou@users.noreply.github.com>
2020-12-03 22:44:05 +0100

    Update substitution.c

--- week2/substitution.c
+++ week2/substitution.c
@@ -1,1 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/*
+ * Author: Lionel Ngoubou
+ * Purpose: Substitution cipher implementation :
+   a program that encrypts messages
+ */
 
