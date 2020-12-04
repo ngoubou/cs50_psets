f551f01d83daac2237bb97554a2c7c5170002e15
Lionel Ngoubou <38511971+ngoubou@users.noreply.github.com>
2020-12-04 18:14:03 +0100

    Update cash.c
    Improved my solution using array and a for loop

--- week1/cash.c
+++ week1/cash.c
@@ -1,55 +1,44 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <math.h>
-
+#include <string.h>
+#include <stdlib.h>
 /*
  * Author: Lionel Ngoubou
  * Purpose: Greedy Algorithm implementation :
  Return the minimum number of coins needed for a change.
  */
 
+const int N = 4;
+
 int main(void)
 {
-    float dollars;
-    
-    // prompt the user to enter a positive float; 
+
+    // Prompt the user to enter a positive float;
     // if he fails to, re-prompt him for a valid input
+    float dollars;
     do
     {
-        
         dollars = get_float("How much: ");
     }
     while (dollars < 0);
- 
+
     // to handle float imprecision, we round the value to 100
     int change = round(dollars * 100);
- 
+
     // as long as the change is greater or equals to the largest coin, use it
     // and count the number of times you do an operation, ie number of coins used
     int coins = 0;
-    while (change >= 25)
+    string array[] = {"25", "10", "5", "1"};
+
+    for (int i = 0; i < N; i++)
     {
-        change = change - 25;
-        coins++;
+        while (change >= atoi(array[i]))
+        {
+            change = change - atoi(array[i]);
+            coins++;
+        }
     }
- 
-    while (change >= 10)
-    {
-        change = change - 10;
-        coins++;
-    }  
- 
-    while (change >= 5)
-    {
-        change = change - 5;
-        coins++;
-    }
- 
-    while (change >= 1)
-    {
-        change = change - 1;
-        coins++;
-    }
-   
-    printf("number of coins: %i\n", coins);
+
+    printf("Number of coins: %i\n", coins);
 }
