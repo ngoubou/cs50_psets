a5b5b8bd70e6c9844148f1b2165d561009aa30c8
Lionel Ngoubou <38511971+ngoubou@users.noreply.github.com>
2020-12-05 23:29:11 +0100

    Update cash.c

--- week1/cash.c
+++ week1/cash.c
@@ -1,44 +1,40 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include <stdlib.h>
+
 /*
  * Author: Lionel Ngoubou
- * Purpose: Greedy Algorithm implementation :
- Return the minimum number of coins needed for a change.
+ * Purpose: Recreate the Super Mario right-aligned pyramid.
  */
-
-const int N = 4;
 
 int main(void)
 {
+    int height;
 
-    // Prompt the user to enter a positive float;
-    // if he fails to, re-prompt him for a valid input
-    float dollars;
     do
     {
-        dollars = get_float("How much: ");
+        // Prompt the user to enter an integer
+        height = get_int("Height: ");
     }
-    while (dollars < 0);
+    while (height < 1 || height > 8);
 
-    // to handle float imprecision, we round the value to 100
-    int change = round(dollars * 100);
 
-    // as long as the change is greater or equals to the largest coin, use it
-    // and count the number of times you do an operation, ie number of coins used
-    int coins = 0;
-    string array[] = {"25", "10", "5", "1"};
-
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < height; i++)
     {
-        while (change >= atoi(array[i]))
+        // Print the space characters
+        for (int j = height - 1 - i; j > 0 ; j--)
         {
-            change = change - atoi(array[i]);
-            coins++;
+            printf(" ");
         }
+        // Print the hashes
+        for (int k = i + 1; k > 0; k--)
+        {
+            printf("#");
+
+        }
+        printf("\n");
+
     }
 
-    printf("Number of coins: %i\n", coins);
 }
+
+
