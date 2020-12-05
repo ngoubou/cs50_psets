470e264d7e35843c95bb83940bacfe0eec51443c
Lionel Ngoubou <38511971+ngoubou@users.noreply.github.com>
2020-12-05 23:37:10 +0100

    Update cash.py
    I initially implemented it in python just to practice my skills
    I later found that this was needed for week 6
    my code returns 7/10 when I submit it so I'll have to improve it when I'm on week 6
    1 easy fix is the while loop repetition

--- week6/cash.py
+++ week6/cash.py
@@ -1,1 +1,33 @@
+# I'm implementing CS50 problem set 1 'cash' in python.
+# This problem is also named the coin change problem
+
+# Prompt the user to enter a number.
+# In python 3, input is a string so i convert it to float
+dollars = float(input("How much:"))
+
+# To handle float imprecision i convert it to an integer and round it
+change = round(dollars * 100)
+
+# I initialize a variable that will keep track of 
+# the number of times the loop is executed.
+# This number represents the minimum of coins for change
+coins = 0
+
+while change >= 25:
+    change = change - 25
+    coins = coins + 1
+    
+while change >= 10:
+    change = change - 10
+    coins = coins + 1
+    
+while change >= 5:
+    change = change - 5
+    coins = coins + 1
+    
+while change >= 1:
+    change = change - 1
+    coins = coins + 1    
+
+print("number of coins is " + str(coins))
 
