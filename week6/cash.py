e0d2e6cd25f38dab1debd0b808639541fd6b1387
Lionel Ngoubou <38511971+ngoubou@users.noreply.github.com>
2020-12-05 23:56:26 +0100

    Update cash.py

--- week6/cash.py
+++ week6/cash.py
@@ -3,31 +3,25 @@
 
 # Prompt the user to enter a number.
 # In python 3, input is a string so i convert it to float
-dollars = float(input("How much:"))
+dollars = float(input("How much: "))
 
 # To handle float imprecision i convert it to an integer and round it
 change = round(dollars * 100)
 
-# I initialize a variable that will keep track of 
+# I initialize a variable that will keep track of
 # the number of times the loop is executed.
 # This number represents the minimum of coins for change
 coins = 0
 
-while change >= 25:
-    change = change - 25
-    coins = coins + 1
-    
-while change >= 10:
-    change = change - 10
-    coins = coins + 1
-    
-while change >= 5:
-    change = change - 5
-    coins = coins + 1
-    
-while change >= 1:
-    change = change - 1
-    coins = coins + 1    
+# Create an array that'll store the coins face values
+array = [25, 10, 5, 1]
+
+# Iterate through the array and subtract the biggest value
+# As long as the change is greater or equaled to the largest coin
+for i in range(len(array)):
+    while change >= array[i]:
+        change = change - array[i]
+        coins = coins + 1
 
 print("number of coins is " + str(coins))
 
