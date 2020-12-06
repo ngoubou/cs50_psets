5828b4f8362642e5f9e0f486a5e032d08e62b51b
Lionel Ngoubou <38511971+ngoubou@users.noreply.github.com>
2020-12-06 01:11:45 +0100

    doo

--- week6/cash.py
+++ week6/cash.py
@@ -1,12 +1,27 @@
 # I'm implementing CS50 problem set 1 'cash' in python.
 # This problem is also named the coin change problem
 
-# Prompt the user to enter a number.
-# In python 3, input is a string so i convert it to float
-dollars = float(input("How much: "))
+# Function to dreplace the dots in input numbers
+# so i can test if it is a valid number
+def rreplace(s, old, new, occurrence):
+    li = s.rsplit(old, occurrence)
+    return new.join(li)
 
-# To handle float imprecision i convert it to an integer and round it
-change = round(dollars * 100)
+
+# Prompt the user to enter a number.
+# If input is negative or not a number, keep asking
+while True:
+    dollars = input("How much: ")
+    temp = rreplace(dollars, '.', '', dollars.count('.'))
+    if not temp.isdigit() or float(dollars) < 0:
+        continue
+    
+    else:
+        break
+    
+# In python 3, input is a string so i convert it to float
+# To handle float imprecision i round it to the 100th
+change = round(float(dollars) * 100)
 
 # I initialize a variable that will keep track of
 # the number of times the loop is executed.
