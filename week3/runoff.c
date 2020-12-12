c59f1219ad8b8b277a3e500ade10f6a7f88ae78a
Lionel Ngoubou <38511971+ngoubou@users.noreply.github.com>
2020-12-12 13:02:44 +0100

    function find_min completed

--- week3/runoff.c
+++ week3/runoff.c
@@ -215,6 +215,13 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false)
+        {
+            printf("%i\n", candidates[i].votes);
+        }
+    }
     // TODO
     return 0;
 }
